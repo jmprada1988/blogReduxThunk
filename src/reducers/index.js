ee0a163c5c1a4b7e3641a859008fd654c9d06206
davirlein1988 <lein.davir@gmail.com>
2019-08-26 00:08:07 -0500

    Completed app

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux'
+import postReducers from './postReducers';
+import usersReducer from './usersReducer';
+
+
+
+
+export default combineReducers({
+    posts: postReducers,
+    users: usersReducer
+})
